e3081b383a5e8ffad80f7584e6aee39ca14a6fdf
broadband <iambroadband@gmail.com>
2023-10-05 23:40:49 -0700

    works with local copy of scooze. need to bugfix some things I found in scooze though.

--- words_per_card.py
+++ words_per_card.py
@@ -2,22 +2,12 @@ import os
 from typing import List
 
 import scooze.bulkdata as bulk
-import scrython
 from scooze.api import ScoozeApi
 from scooze.catalogs import ScryfallBulkFile
 from scooze.deck import Deck
 
+
 DOWNLOAD_CARDS = False
-
-
-def word_count(card: scrython.cards.named.Named) -> int:
-    # Non-MDFC
-    try:
-        return len(card.oracle_text().split(" "))
-    # MDFC
-    except KeyError:
-        return sum([len(face["oracle_text"].split(" ")) for face in card.card_faces()])
-
 
 def main():
     with ScoozeApi() as scooze:
@@ -30,28 +20,27 @@ def main():
 
         for filename in os.scandir(deck_dir):
             if filename.is_file():
-                print(filename.path)
-                if filename.path.endswith("Rakdos Midrange.txt"): # FIXME: this is a test to only use Rakdos Midrange because flip cards aren't working in scooze
-                    with open(filename.path) as deck_file:
-                        deck = Deck(
-                            archetype=os.path.basename(filename.path).split(".")[0],
-                        )
+                # print(filename.path)
+                with open(filename.path) as deck_file:
+                    deck = Deck(
+                        archetype=os.path.basename(filename.path).split(".")[0],
+                    )
 
-                        for line in deck_file:
-                            if not line.isspace():
-                                # print(line)
-                                (quantity, card_name) = line.split(" ", 1)
-                                quantity = int(quantity)
-                                card_name = card_name.strip()
-                                card = scooze.get_card_by_name(card_name)
-                                deck.add_card(card, quantity)
+                    for line in deck_file:
+                        if not line.isspace():
+                            # print(line)
+                            (quantity, card_name) = line.split(" ", 1)
+                            quantity = int(quantity)
+                            card_name = card_name.strip()
+                            card = scooze.get_card_by_name(card_name)
+                            deck.add_card(card, quantity)
 
-                        decks.append(deck)
+                    decks.append(deck)
 
         # TODO: could sort here
-        print("Deck Archetype\tTotal Words\tAverage Words")
+        print(f"{'Deck Archetype':30s}{'Total Words':30s}{'Average Words':30s}")
         for deck in decks:
-            print(f"{deck.archetype}\t{deck.total_words()}\t\t{deck.average_words()}")
+            print(f"{deck.archetype:30s}{deck.total_words()}\t\t\t\t{deck.average_words():4.2f}")
 
 
 if __name__ == "__main__":
